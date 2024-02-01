d16c3fc07d97f1a4fed11564e7ad925c95bc6b65
danpu323 <danpu323@student.liu.se>
2024-02-01 15:39:24 +0100

    added comment chopping wood and fenwick

--- exercises/choppingwood.cpp
+++ exercises/choppingwood.cpp
@@ -3,11 +3,13 @@
 #include <sstream>
 #include <iterator>
 #include <array>
+#include <set>
 using namespace std;
 
 
 /**
  * @brief Main function, takes input from terminal and outputs answer to terminal.
+ * Takes a V list and tries to compute the U list.
  *
  * @return int
  */
@@ -18,34 +20,68 @@ int main() {
     getline(cin, input);
     int num_nodes = stoi(input);
 
+    // No input got
     if(num_nodes == 0) {
         cout << "Error" << "\n";
-        return;
+        return 0;
     }
 
+    // Make basic lists
     vector<int> degree(num_nodes+1);
-    vector<int> u(num_nodes+1);
+    vector<int> v(num_nodes);
 
-
+    // Take out numbers from terminal and put them in degree and v.
     for(int i = 0; i < num_nodes; i++) {
         getline(cin, input);
         int curr_node = stoi(input);
-        degree[curr_node] += 1;
-        if(i == num_nodes -1 && curr_node != num_nodes + 1) {
+
+        // We have a bigger input than should exist
+        if(curr_node > (num_nodes + 1)) {
             cout << "Error" << "\n";
-            return;
+            return 0;
         }
+
+        // Add to v
+        v[i] = curr_node;
+
+        // Add to degrees (-1 for index starts at 0)
+        int deg_index = curr_node - 1;
+        degree[deg_index] += 1;
+
     }
 
-    for(int i = 0; i < num_nodes + 1; i++) {
+    // Last element in the input stream is not the biggest node.
+    if(v[v.size()-1] != (num_nodes + 1)) {
+        cout << "Error" << "\n";
+        return 0;
+    }
+
+    // Insert leaves (nodes that have degree 0)
+    set<int> leaves; // Set sorts leaves, smallest leaf first
+    for(unsigned i = 0; i < degree.size(); i ++) {
         if(degree[i] == 0){
-            degree[i] -= 1;
-            u.push_back(i);
-            i = 0;
+            leaves.insert(i);
         }
     }
 
+    // Make the u list
+    int k = 0;
+    while(leaves.size() != 0 && k < num_nodes) {
+        // Take top (smalles) leaf and remove it from leaves
+        int top_leaf = *leaves.begin();
+        leaves.erase(top_leaf);
 
-
-    return;
+        // Print smallest leaf
+        cout << to_string(top_leaf + 1) << "\n";
+        // Get new degree
+        int upd_deg = degree[v[k]-1] - 1;
+        // Update leaves if needed
+        if(upd_deg == 0) {
+            leaves.insert(v[k]-1);
+        }
+        // Update degree list
+        degree[v[k]-1] = upd_deg;
+        k++;
+    }
+    return 0 ;
 }
--- lab1/fenwick_tree.cpp
+++ lab1/fenwick_tree.cpp
@@ -1,3 +1,24 @@
+/**
+ * @file fenwick_tree.cpp
+ * @author Daniel Purgal, danpu323 (danpu323@student.liu.se)
+ * @brief Takes in operations and executes them on a fenwick tree.
+ * @version 0.1
+ * @date 2024-02-01
+ *
+ * A fenwick tree is a array/list of length N that can be represented as a tree.
+ * It is done by combining the sum of different parts of the array and storing them in
+ * indexes of powers of 2. The first part is to pair up elements of index 2^0,
+ * meaning 1-2, 3-4, 5-6 and so on get paird up. This means that 2 has sum of 1 and 2, 4 has sum om 3 and 4,
+ * but 1,3,5 has thier own value. Then indexes 2^1: 2-4, 6-8 and so on.
+ * This goes on till the 2^M where M is the biggest power of 2 in N.
+ *
+ * This property gives it the ability to sum up values up to index i with complexity O(Nlog(N)).
+ * It also has complexity of O(Nlog(N)) when changing/updating a value in the tree.
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
 #include <iostream>
 #include <vector>
 #include <sstream>
@@ -16,56 +37,77 @@ vector<string> separate_string (string str) {
     return { istream_iterator<string>{ ss }, istream_iterator<string>{}  };
 }
 
-
-long sum_fenwick(vector<long>fenwick_tree, int index) {
-    // Increment index because fenwick starts at index 1 not 0.
+/**
+ * @brief Sums all elements up to index index. Has time complexity O(Nlog(N)).
+ *
+ * @param index
+ * @return long
+ */
+long sum_fenwick(vector<long>* fenwick_tree, int index) {
     long sum = 0;
+    // Sum tills we cannot sum anymore (smalles bigroup summed)
     while(index > 0) {
-        sum += fenwick_tree[index];
+        sum += (*fenwick_tree)[index];
         // Flip the lowest significant 1 to a 0. This acts as going to a lower layer of the tree.
         index -= (index & (-index));
     }
     return sum;
 }
 
-vector<long> add_fenwick(vector<long> fenwick_tree, int fenwick_length, int index, int add_value) {
+/**
+ * @brief Updates the fenwick tree by adding a value to position index. Has time complexity O(Nlog(N)).
+ *
+ * @param fenwick_tree
+ * @param fenwick_length
+ * @param index
+ * @param add_value
+ * @return void
+ */
+void update_fenwick(vector<long>* fenwick_tree, int fenwick_length, int index, int add_value) {
     // Increment index because fenwick starts at index 1 not 0
     index++;
+    // Stop when we have gone to the highest layer of the tree (cannot be longer than actual length)
     while(index < fenwick_length) {
-        fenwick_tree[index] += add_value;
+        (*fenwick_tree)[index] += add_value;
         // Step to a higher layer that uses sum of position current index.
         index += (index & (-index));
     }
-    return fenwick_tree;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     string input;
+    // Get first input (N, Q) were N is length of fenwick vector and Q numer of operations
     getline(cin, input);
     vector<string> split_input = separate_string(input);
-    string arg1 = split_input[0];
-    string arg2 = split_input[1];
+    string N = split_input[0];
+    string Q = split_input[1];
     // Fenwick structure start at intiger 1 instead of 0
-    int fenwick_length = stoi(arg1) + 1;
-    int num_operations = stoi(arg2);
+    int fenwick_length = stoi(N) + 1;
+    int num_operations = stoi(Q);
 
     vector<long> fenwick_tree(fenwick_length+1, 0);
-    
+
+    // Compute operations for each line
     for(int i = 0; i < num_operations; i++) {
         getline(cin, input);
         split_input = separate_string(input);
-        // Two cases, one where
+        // Sum operation is used
         if(split_input[0] == "?") {
+            // Get input parameter
             int index = stoi(split_input[1]);
-            long sum = sum_fenwick(fenwick_tree, index);
+            // Get sum up to index index
+            long sum = sum_fenwick(&fenwick_tree, index);
+            // Print to terminal
             cout << sum << "\n";
-
+        // Update vector by addinga integer
         } else {
+            // Get input parameters
             int index = stoi(split_input[1]);
             long add_value = stoi(split_input[2]);
-            fenwick_tree = add_fenwick(fenwick_tree, fenwick_length, index, add_value);
+            // Update the tree
+            update_fenwick(&fenwick_tree, fenwick_length, index, add_value);
         }
     }
 }
