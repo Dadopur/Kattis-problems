9fabaa44dcdcddac62f0089a75043285a4064bed
Daniel Purgal <danpu323@student.liu.se>
2024-02-05 00:20:03 +0100

    power is short

--- exercises/perfect_powers_short.py
+++ exercises/perfect_powers_short.py
@@ -1,18 +1,15 @@
 import math
 while(1):
- x=int(input())
- n=0
- if(x==0):break
- elif(x>0):n=1
- x=abs(x)
- b=1
+ x,n=int(input()),0
+ if x==0:break
+ elif x>0:n=1
+ x,b=abs(x),1
  while b<=math.sqrt(x):
   b+=1
   p=1
   while p<33:
-   if n: p+=1
-   else: p+=2
+   p+=1 if n else 2
    if math.pow(b,p)==x:
-    b=math.inf
+    b=x
     print(p)
- if b!=math.inf:print(1)
+ if b!=x:print(1)
