1cb59326561bdf6a9f1a87f662f60a76b3a25d49
Daniel Purgal <danpu323@student.liu.se>
2024-02-06 11:40:47 +0100

    interval covers almost working

--- lab1/interval_covers.cpp
+++ lab1/interval_covers.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <tuple>
 #include <algorithm>
-#include <limits>
+//#include <limits>
+#include <cfloat>
 using namespace std;
 
 
-bool interval_compare(tuple<double, double, int> i1, tuple<double, double, int> i2) {
+bool interval_compare(tuple<double, double, int>& i1, tuple<double, double, int>& i2) {
     return (get<0>(i1) < get<0>(i2));
 }
 
@@ -16,40 +17,38 @@ vector<int> count_intervals(tuple<double, double> interval, vector<tuple<double,
     double primary_end = get<1>(interval);
     double start = primary_start;
     double end = start - 1;
-    int counts = 0;
     vector<int> indexes;
 
     // Sort interval with complexity O(NlogN) on start value
     sort(intervals.begin(), intervals.end(), interval_compare);
+
     //intervals.push_back(make_tuple(numeric_limits<double>::infinity(), numeric_limits<double>::infinity(), -1));
+    intervals.push_back(make_tuple(DBL_MAX, DBL_MAX, -1));
     
-    for (unsigned i = 0; i < intervals.size(); i++) {
+    int index = -1;
+    for (unsigned i = 0; i < intervals.size();) {
         tuple<double, double, int> new_interval = intervals[i];
         double new_start = get<0>(new_interval);
         double new_end = get<1>(new_interval);
-        int index = get<2>(new_interval);
-
-        tuple<double, double, int> next_interval = intervals[i+1];
-        double next_end = get<1>(next_interval);
-
-        //cout << "START"<< new_start <<" " <<next_end <<endl;
 
         // Update end, is it a better interval or worse?
         if (new_start <= start) {
-            end = max(end, next_end);
-            indexes.push_back(index);
+            end = max(new_end, end);
+            index = get<2>(new_interval);
+            i++;
         } else {
             // Current interval doesn't cover the start, need a new interval
             start = end;
 
+            if (index != -1) { indexes.push_back(index);}
             // End of loop, impossible or done
-            if(new_start > end || end >= primary_end) {
+            if (new_start > end || end >= primary_end) {
                 break;
             }
         }
     }
 
-    // Return
+    // Returns
     if (end < primary_end) {
         // Can't find valid intervals to cover interval.
         vector<int> invalid = {-1};
@@ -67,19 +66,17 @@ int main(){
     // Get interval to be checked
     double start_interval;
     double end_interval;
-    while(cin >> start_interval) {
-        cin >> end_interval;
+    while(cin >> start_interval >> end_interval) {
         tuple<double, double> interval = make_tuple(start_interval, end_interval);
 
         // Number of intervals that will be given. 
         int num_intervals;
         cin >> num_intervals;
-        cout <<"STARTING "<< num_intervals << endl;
+        //cout <<"STARTING "<< num_intervals << endl;
         // Create intervals vector
         vector<tuple<double, double, int>> intervals;
         for (int i = 0; i < num_intervals; i++) {
-            cin >> start_interval;
-            cin >> end_interval;
+            cin >> start_interval >> end_interval;
             intervals.push_back(make_tuple(start_interval, end_interval, i));
         }
         // Find number of counts
