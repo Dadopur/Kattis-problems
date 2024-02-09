a5e5baa81fb0030eb0829787d2d5f3dfeb6cfe8b
Daniel Purgal <danpu323@student.liu.se>
2024-02-09 15:30:08 +0100

    fill grid wrong

--- lab1/knapsack.cpp
+++ lab1/knapsack.cpp
@@ -12,60 +12,65 @@ struct Item
 vector<int> knapsack(vector<Item> items, int capacity) {
     int num_items = items.size();
 
-    vector<vector<int>> grid(num_items+1, vector<int>(capacity, 0));
+    vector<vector<int>> grid(num_items+1, vector<int>(capacity+1, 0));
     
     // Fill the grid (the sack with all cases)
     for(auto item : items) {
         for(int curr_capacity = 1; curr_capacity <= capacity; curr_capacity++) {
             // Initialize useful variables
-            int value_of_pos = 0; // default value 0
             int item_index = item.index;
             int item_value = item.value;
             int item_weight = item.weight;
-
-            cout <<"index "<< item_index << "value " << item_value << "weight " << item_weight << endl;
+            int value_of_pos = grid[item_index-1][curr_capacity]; // default value of prev grid same capacity
+            
             // Position value should not be 0
             if(curr_capacity >= item_weight) {
                 // Update with prev grid value on best path
-                int prev_grid_side_value = grid[item_index-1][capacity-item_weight];
-                int prev_grid_value = grid[item_index-1][capacity];
-                cout << "FETT GAY" << endl;
-                if(prev_grid_side_value > prev_grid_value){
-                    value_of_pos = item_value + prev_grid_side_value;
-                } else {
-                    value_of_pos = prev_grid_value;
+                int prev_grid_side_value = grid[item_index-1][curr_capacity-item_weight] + item_value;
+                //cout << prev_grid_side_value << " " << prev_grid_value << endl;
+                if(prev_grid_side_value > value_of_pos){
+                    value_of_pos = prev_grid_side_value;
                 }
             }
-            cout << "val " << value_of_pos<< endl;
             // Update grid value
             grid[item_index][curr_capacity] = value_of_pos;
         }
     }
-    
-    for(auto i : grid){
-        for(int l : i) {
-            cout << l << endl;
+
+    for(auto lala : grid){
+        for(int la : lala){
+            cout << la << endl;
         }
     }
 
     vector<int> items_in_sack;
     int curr_capacity = capacity;
     // Back tracking to find optimal path.
-    for(int index = num_items; index > 0; index--) {
-        Item item = items[index-1];
+    for(int index = num_items-1; index >= 0; index--) {
+        Item item = items[index];
         int weight = item.weight;
 
-        int curr_value = grid[index][curr_capacity];
-        //cout << "HERE "<<weight<<" "<<index << endl;
-        if(curr_capacity >= weight) {
-            int next_value_side = grid[index-1][curr_capacity-weight];
-            int next_value_straight = grid[index-1][curr_capacity];
-
+        int curr_value = grid[index+1][curr_capacity];
+        if(curr_capacity > weight) {
+            cout << "inside first layer " << curr_capacity <<" weight "<< weight << endl;
+            int next_value_side = grid[index][curr_capacity-weight];
+            int next_value_straight = grid[index][curr_capacity];
+            cout<< "bajs "<< next_value_side << " "<<next_value_straight << endl;
             if(next_value_side > next_value_straight) {
+                cout << "before " << curr_capacity << endl;
                 curr_capacity -= weight;
+                cout << "after " << curr_capacity << endl;
                 items_in_sack.push_back(index);
             }
+        } else if(curr_capacity == weight) {
+            items_in_sack.push_back(index);
+            break;
         }
+    }
+    cout << "banan" << endl;
+    for(auto i : items_in_sack){
+        cout <<"here" << endl;
+        cout << i << endl;
     }
     return items_in_sack;
 }
@@ -88,6 +93,7 @@ int main() {
             items.push_back(t);
         }
         vector<int> items_in_sack = knapsack(items, capacity);
+            cout << "HERE "<< endl; 
         cout << items_in_sack.size() << "\n";
         for(int item : items_in_sack) {
             cout << item << "\n";
