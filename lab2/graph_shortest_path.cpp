2b30a897ec269bfddfe6b7000b8e76482c7dd37e
Daniel Purgal <danpu323@student.liu.se>
2024-02-21 20:06:45 +0100

    added new node structure

--- lab2/graph_shortest_path.cpp
+++ lab2/graph_shortest_path.cpp
@@ -5,18 +5,72 @@
 #include <algorithm>
 using namespace std;
 
-struct Node {
-    Node(const int in_index) : index(in_index) {}
-    int index;
-    // add a value to this
-    // change to pair instead of 2 seperate vectors
-    vector<Node*> connections;
-    vector<int> edge_costs;
-    ~Node(){
-        for(auto node : connections){
-            delete node;
-        }
+struct Node;
+
+struct Edge {
+    // Constructor
+    Edge(Node* node, const int cost) : connection_node(node), edge_cost(cost) {}
+    // Destructor
+    ~Edge() {
+        delete connection_node;
     }
+    // Variables
+    const Node* connection_node;
+    const int edge_cost; 
+};
+
+class Node {
+    public:
+        // Constructor
+        Node(const int in_index) : index(in_index), visited(false), value(0) {}
+        // Destructor
+        ~Node(){
+            for(auto edge : edges){
+                delete edge;
+            }
+        }
+        // Getters
+        // ================================
+        int get_index() {
+            return index;
+        }
+
+        int get_value() {
+            return value;
+        }
+
+        bool is_visited() {
+            return visited;
+        }
+
+        vector<Edge*> get_edges() {
+            return edges;
+        }
+
+        Edge* get_edge(int edge_index) {
+            return edges[edge_index];
+        }
+
+        // Setters
+        // ================================
+        void set_value(int new_value) {
+            value = new_value;
+        }
+
+        void set_visited(bool new_visited) {
+            visited = new_visited;
+        }
+
+        void add_edge(Edge* new_edge) {
+            edges.push_back(new_edge);
+        }
+
+    private:
+        // Variables
+        const int index;
+        int value;
+        bool visited;
+        vector<Edge*> edges;
 };
 
 bool node_value_compare(pair<int, int> node1, pair<int, int> node2) {
@@ -41,25 +95,37 @@ vector<pair<int, int>> dijkstra(vector<Node*>& nodes, int start_node_index) {
     return table;
 } 
 
-void add_node(vector<Node*>& nodes, int node1, int node2, int weight) {
-    // Add nodes if not initialized
-    if(nodes[node1] == nullptr) {
-        nodes[node1] = new Node{node1};
-    }
-
-    if(nodes[node2] == nullptr) {
-        nodes[node2] = new Node{node2};
-    }
-
-    // Add connections and edge costs
+/**
+ * @brief Adds a new connection between two nodes with a given weight cost.
+ * 
+ * @param nodes Vector of all nodes in the graph.
+ * @param node1 Index of first node to be connected.
+ * @param node2 Index of second node to be connected.
+ * @param cost Path cost for the connection (edge).
+ */
+void add_edge(vector<Node*>& nodes, int node1, int node2, int cost) {
+    // Get nodes to be connected
     Node* primary_node = nodes[node1];
     Node* secundary_node = nodes[node2];
     
-    primary_node->connections.push_back(secundary_node);
-    secundary_node->connections.push_back(primary_node);
+    // Make new edges 
+    Edge* edge1 = new Edge{primary_node, cost};
+    Edge* edge2 = new Edge{secundary_node, cost};
 
-    primary_node->edge_costs.push_back(weight);
-    secundary_node->edge_costs.push_back(weight);
+    // Add new edges to the nodes
+    primary_node->add_edge(edge1);
+    secundary_node->add_edge(edge2);
+}
+
+/**
+ * @brief Initializes nodes with corresponding index.
+ * 
+ * @param nodes Vector of length N (number of nodes) to be initialized with nodes pointers.
+ */
+void init_nodes(vector<Node*>& nodes) {
+    for(int i = 0; i < nodes.size(); i++) {
+        nodes[i] = new Node{i};
+    }
 }
 
 
@@ -70,12 +136,13 @@ int main(){
 
     int n, m, q, s;
     while((cin >> n >> m >> q >> s) && !(n==0 && m==0 && q==0 && s==0)) {
-        // Init with empty nodes instead of nullptr:s
-        vector<Node*> nodes(n, nullptr);
+        vector<Node*> nodes(n);
+        init_nodes(nodes);
+
         int node1, node2, weight;
         for(int i = 0; i < m; i++) {
             cin >> node1 >> node2 >> weight;
-            add_node(nodes, node1, node2, weight); 
+            add_edge(nodes, node1, node2, weight); 
         }
 
     }
