31654d2f40c2ad2185fcf09788b5cf1b7af4988a
danpu323 <danpu323@student.liu.se>
2024-02-22 11:47:15 +0100

    add graph class and const values

--- lab2/graph_shortest_path.cpp
+++ lab2/graph_shortest_path.cpp
@@ -1,15 +1,14 @@
 /**
  * @file graph_shortest_path.cpp
  * @author Daniel Purgal, danpu323 (danpu323@student.liu.se)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2024-02-21
- * 
+ *
  */
 #include <iostream>
 #include <vector>
 #include <limits>
-#include <utility>
 #include <algorithm>
 using namespace std;
 
@@ -17,18 +16,10 @@ class Node;
 
 struct Edge {
     // Constructor
-    Edge(Node* node, const int cost) : connection_node(node), edge_cost(cost) {}
-    
-    
-    // Destructor
-    // ~Edge() {
-    //     delete connection_node;
-    // }
-
-
-    // Variables
+    Edge(Node* const node, const int cost) : connection_node(node), edge_cost(cost) {}
+    // Member variables
     const Node* connection_node;
-    const int edge_cost; 
+    const int edge_cost;
 };
 
 class Node {
@@ -43,46 +34,62 @@ class Node {
         }
         // Getters
         // ================================
-        int get_index() {
+        int get_index() const {
             return index;
         }
 
-        int get_value() {
+        int get_value() const {
             return value;
         }
 
-        bool is_visited() {
+        bool is_visited() const {
             return visited;
         }
 
-        vector<Edge*> get_edges() {
+        vector<Edge*> get_edges() const {
             return edges;
         }
 
-        Edge* get_edge(int edge_index) {
+        Edge* get_edge(int const edge_index) const {
             return edges[edge_index];
         }
 
         // Setters
         // ================================
-        void set_value(int new_value) {
+        void set_value(int const new_value) {
             value = new_value;
         }
 
-        void set_visited(bool new_visited) {
+        void set_visited(bool const new_visited) {
             visited = new_visited;
         }
 
-        void add_edge(Edge* new_edge) {
+        void add_edge(Edge* const new_edge) {
             edges.push_back(new_edge);
         }
 
     private:
-        // Variables
+        // Member variables
         const int index;
         int value;
         bool visited;
         vector<Edge*> edges;
+};
+
+class Graph {
+    public:
+        ~Graph() {
+            for(Node* node : nodes) {
+                delete node;
+            }
+        }
+
+        Node* get_node(int const index) const {
+            return nodes[index];
+        }
+
+    private:
+        vector<Node*> nodes;
 };
 
 bool node_value_compare(pair<int, int> node1, pair<int, int> node2) {
@@ -93,11 +100,11 @@ vector<pair<int, int>> dijkstra(vector<Node*>& graph, int start_node_index) {
 
     Node* start_node = graph[start_node_index];
     start_node->set_value(0);
-    
+
     // Add all node pointers to the set comparing them on distance, dont forget to remove and re-add nodes with updated value
 
 
-    // Pair of (prev_index, curr_value) 
+    // Pair of (prev_index, curr_value)
     vector<pair<int, int>> table(graph.size(), {0, numeric_limits<int>::max()});
     vector<int> visited_nodes;
 
@@ -105,18 +112,18 @@ vector<pair<int, int>> dijkstra(vector<Node*>& graph, int start_node_index) {
     table[start_node_index].second = 0;
 
     // Go through all edges in start node and update next node_index (value = edge_cost + curr_value)
-    // Add curr node index to visited nodes, go to next node in 
+    // Add curr node index to visited nodes, go to next node in
     // dont go back and update/ check already visited nodes.
 
     // Use a set, the we need to have a tuple/ 3 long array cuz index will change.
     vector<pair<int, int>>::iterator next_node = min_element(table.begin(), table.end(), node_value_compare);
 
     return table;
-} 
+}
 
 /**
  * @brief Adds a new connection between two nodes with a given weight cost.
- * 
+ *
  * @param nodes Vector of all nodes in the graph.
  * @param node1 Index of first node to be connected.
  * @param node2 Index of second node to be connected.
@@ -126,8 +133,8 @@ void add_edge(vector<Node*>& graph, int node1, int node2, int cost) {
     // Get nodes to be connected
     Node* primary_node = graph[node1];
     Node* secundary_node = graph[node2];
-    
-    // Make new edges 
+
+    // Make new edges
     Edge* edge1 = new Edge{primary_node, cost};
     Edge* edge2 = new Edge{secundary_node, cost};
 
@@ -138,7 +145,7 @@ void add_edge(vector<Node*>& graph, int node1, int node2, int cost) {
 
 /**
  * @brief Initializes nodes with corresponding index.
- * 
+ *
  * @param nodes Vector of length N (number of nodes) to be initialized with nodes pointers.
  */
 void init_nodes(vector<Node*>& graph, int init_value) {
@@ -150,8 +157,8 @@ void init_nodes(vector<Node*>& graph, int init_value) {
 /**
  * @brief Main function that takes inputs and outputs to the consol.
  * Finds the shortest (lowest cost) path to a given node in a given graph.
- * 
- * @return int 
+ *
+ * @return int
  */
 int main(){
     ios_base::sync_with_stdio(false);
@@ -166,7 +173,7 @@ int main(){
         int node1, node2, weight;
         for(int i = 0; i < m; i++) {
             cin >> node1 >> node2 >> weight;
-            add_edge(graph, node1, node2, weight); 
+            add_edge(graph, node1, node2, weight);
         }
 
     }
